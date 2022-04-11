8e378b984842a620848c97d14c5cc596351c827b
ppalniraj1996 <97506350+ppalniraj1996@users.noreply.github.com>
2022-04-11 13:00:45 +0530

    eval

--- src/components/GroceryItem.jsx
+++ src/components/GroceryItem.jsx
@@ -1,5 +1,18 @@
-// reusable card component
-const GroceryItem = () => {
-  return <></>;
+import React from "react";
+import "./Style.css"
+
+const GroceryItem = (props) => {
+  console.log(props)
+  const {title,imgURL,discount,sellingPrice,mrp} =props
+  return (
+  <div id="main">
+    <img src={imgURL} alt=""/>
+    <h1>{title}</h1>
+    <h1>discount:{discount}</h1>
+    <h1>MRP:{mrp}</h1>
+    <h1>Price:{sellingPrice}</h1>
+
+  </div>
+  )
 };
 export default GroceryItem;
--- src/components/Grocerydetails.jsx
+++ src/components/Grocerydetails.jsx
@@ -1,14 +1,23 @@
 import React from "react"
 import data from '../data.json'
+import GroceryItem from "./GroceryItem"
+
+
 
 
 const GroceryDetails = ()=>{
-console.log(data)
+// console.log(data)
 
     return(
         <>
         <h1>Groceries</h1>
         <div className="container">
+            {data.map((item,index)=>{
+                return(
+                  <GroceryItem key ={item.id} {...item}/>
+                )
+               
+            })}
             {/* map through the data and display the cards */}
         </div>
         </>
